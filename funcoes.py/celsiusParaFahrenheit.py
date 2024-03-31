b6b3c8093b1047130cb7e2d9b4cff50ae2b80785
MicaeleGomes <micaellyvitoriac@gmail.com>
2024-03-31 14:46:36 -0300

    Adicionando exercicios de funcoes

---
+++ funcoes.py/celsiusParaFahrenheit.py
@@ -0,0 +1,13 @@
+'''
+Faça uma função que converte temperaturas em Celsius para Fahrenheit.
+O nome da sua função deve ser celsius_para_fahrenheit
+'''
+
+#C = (F -32) / 1.8
+#F = (1.8 * C) + 32
+
+def celsius_para_fahrenheit (tempCelsius):
+    
+    tempFahrenheit = (1.8 * tempCelsius) + 32
+
+    return tempFahrenheit
