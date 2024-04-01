905a611712d50fa0b968bd3596e75bd1e828144a
MicaeleGomes <micaellyvitoriac@gmail.com>
2024-03-31 21:48:22 -0300

    Adicionando exercicios de funcoes

---
+++ funcoes.py/areaDoTriangulo.py
@@ -0,0 +1,12 @@
+''''
+Faça uma função que calcula a área de um triângulo de base b e altura h.
+
+O nome da sua função deve ser calcula_area_do_triangulo
+'''
+
+#formula: (b * h) / 2
+
+def calcula_area_do_triangulo (b, h):
+    area = (b * h) / 2
+
+    return area
