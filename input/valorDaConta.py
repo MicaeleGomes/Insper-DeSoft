98e19cb50a44169fedc48fcef3cd44b6f03ee82f
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2024-09-28 14:26:21 -0300

    valorDaConta.py

--- input/valorDaConta.py
+++ input/valorDaConta.py
@@ -1,3 +1,6 @@
+# Escreva um programa que pergunta para o usuário o valor da conta do restaurante e imprime: "Valor da conta com 10%: R$ X.YZ", 
+# onde X.YZ é o valor da conta mais os 10% com exatamente duas casas decimais.
+
 valor_conta = float(input("Qual foi o valor da conta?: "))
 
 valor_total = valor_conta + (0.10 * valor_conta)
