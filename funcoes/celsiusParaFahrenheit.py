1847e32e4f9f64d999ba2028babe69e483bff89f
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2024-09-28 14:32:20 -0300

    celsiusParaFahrenheit.py

---
+++ funcoes/celsiusParaFahrenheit.py
@@ -0,0 +1,7 @@
+#Faça uma função que converte temperaturas em Celsius para Fahrenheit.
+#Sua função deve se chamar celsius_para_fahrenheit.
+
+def celsius_para_fahrenheit (tempCelsius):
+    tempFahrenheit = (1.8 * tempCelsius) + 32 
+
+    return tempFahrenheit
