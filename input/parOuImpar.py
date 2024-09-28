ea490d29ffddcad9dde47d96cbe511dbfd6531e6
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2024-09-28 14:25:22 -0300

    parOuImpar.py

--- input/parOuImpar.py
+++ input/parOuImpar.py
@@ -1,3 +1,6 @@
+# Faça uma função que receba um número inteiro como argumento e devolva 1 se esse número for par ou -1 se o número for ímpar.
+# Sua função deve se chamar verifica_par_ou_impar.
+
 def verifica_par_ou_impar(numero):  
 
     if numero % 2 == 0:
