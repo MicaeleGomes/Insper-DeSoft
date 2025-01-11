8a8ac637544e58a8cc19bc4da0567b0a037faa2b
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2025-01-10 22:45:35 -0300

    Update areaDoTriangulo.py

--- funcoes/dificuldade_1/areaDoTriangulo.py
+++ funcoes/dificuldade_1/areaDoTriangulo.py
@@ -1,3 +1,6 @@
+# Faça uma função que calcula a área de um triângulo de base b e altura h, ela deve receber os valores nesta ordem.
+# Sua função deve se chamar calcula_area_do_triangulo.
+
 def calcula_area_do_triangulo (b, h): 
     areaTriangulo = b * h / 2
     return areaTriangulo
