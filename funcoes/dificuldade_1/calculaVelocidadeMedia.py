a51b78e8f3d9a937f5a69af6535f9fe0299d2786
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2025-01-10 22:37:21 -0300

    Create calculaVelocidadeMedia.py

---
+++ funcoes/dificuldade_1/calculaVelocidadeMedia.py
@@ -0,0 +1,9 @@
+# Escreva uma função que recebe um valor de distância, em km, percorrida por um veículo e o tempo gasto, em horas, 
+# para percorrer esta distância. A função retorna a velocidade média do veículo em km/h.
+# Sua função deve se chamar calcula_velocidade_media
+
+def calcula_velocidade_media (distanciaKm, tempHoras): 
+
+    velocidadeMedia = distanciaKm/tempHoras
+
+    return velocidadeMedia
