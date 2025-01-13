80252ffb3e19533531c0a7e5d63aba8e95a73690
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2025-01-12 21:40:38 -0300

    Create areaDoCirculo.py

---
+++ funcoes/dificuldade_1/areaDoCirculo.py
@@ -0,0 +1,10 @@
+# Faça uma função que recebe o raio de um círculo e devolve a sua área.
+# O nome da sua função deve ser calcula_area_do_circulo.
+
+import math 
+
+def calcula_area_do_circulo (r): 
+
+    area = math.pi * (r**2)
+
+    return area
