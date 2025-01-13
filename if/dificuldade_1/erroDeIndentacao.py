e1e443ea69c76583ffe5db5782ef65d2f5e96342
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2025-01-12 21:45:11 -0300

    Create erroDeIndentacao.py

---
+++ if/dificuldade_1/erroDeIndentacao.py
@@ -0,0 +1,15 @@
+# Conserte o erro de indentação do código:
+
+# def x_maior_que_y(x, y):
+#         if x > y:
+#             resultado = 1
+#         return resultado
+
+#     print(x_maior_que_y(10, 5))
+
+def x_maior_que_y(x, y):
+    if x > y:
+        resultado = 1
+    return resultado
+
+print(x_maior_que_y(10, 5))
