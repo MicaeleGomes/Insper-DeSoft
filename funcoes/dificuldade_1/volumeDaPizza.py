2e8b2b6cf6e94ab54dcdb7fa17d146cb3ff039c2
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2025-01-12 21:41:54 -0300

    Create volumeDaPizza.py

---
+++ funcoes/dificuldade_1/volumeDaPizza.py
@@ -0,0 +1,10 @@
+# Faça uma função que calcule o volume de uma pizza de raio z e altura a. Assuma que a pizza é um cilindro.
+# Sua função deve se chamar volume_da_pizza.
+
+import math 
+
+def volume_da_pizza (z, a): 
+
+    volume = math.pi * z**2 * a
+
+    return volume 
