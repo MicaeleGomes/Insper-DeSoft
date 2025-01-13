a29e76c3ecbb848d3c1d6245b78d39b00f13de05
Micaele Gomes <98981959+MicaeleGomes@users.noreply.github.com>
2025-01-12 21:15:29 -0300

    Create calculaOVolumeDaEsfera.py

---
+++ funcoes/dificuldade_1/calculaOVolumeDaEsfera.py
@@ -0,0 +1,9 @@
+# Faça uma função que calcule o volume de uma esfera de raio R. Sua função deve se chamar calcula_volume_da_esfera.
+
+import math
+
+def calcula_volume_da_esfera(r): 
+
+    v = (4/3) * math.pi * (r**3)
+
+    return v
